4d06e5d7135024ad4a03b6c42fa9cbf7d5802fa4
Raphael Cohen <raphael.cohen@sekoia.fr>
2024-10-29 14:30:59 +0100

    feat: Update module

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from sekoia_automation.module import Module
 
 from http_module.download_file_action import DownloadFileAction
 from http_module.request_action import RequestAction
+from http_module.new_entries_trigger import NewEntriesTrigger
 
 if __name__ == "__main__":
     module = Module()
 
     module.register(DownloadFileAction, "download-file")
     module.register(RequestAction, "request")
+    module.register(NewEntriesTrigger, "new-entries")
 
     module.run()
